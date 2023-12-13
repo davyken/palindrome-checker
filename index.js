969d7778a5e4d5d83a29ca6ec3f8d7899432aa7c
davyken <davykennang552@example.com>
2023-12-13 12:51:08 +0100

    modified content

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-function palindromeTest(str) {
-    let l = str.length - 1
-    for (let i = 0; i < l / 2; i++) {
-        if (str[i] !== str[l]) {
-            return false
-        }
-        l--
+ function palindromeTest(str) {
+  let l = str.length - 1
+  for (let i = 0; i < l / 2; i++) {
+      if (str[i] !== str[l]) {
+    return false
     }
-    return true
+    l--
+  }
+  return true
 }
-function checkPalindrome() {
-    const pali = document.getElementById('text-area').value
-    const Ispalindrome = palindromeTest(pali)
-    const E = document.getElementById('result')
-    if (Ispalindrome) {
-        E.innerHTML = 'Correct, it is a Palindrome !'
-    } else {
-        E.innerHTML = 'Sorry, it is not a Palindrome !'
-    }
+ function checkPalindrome() {
+  const pali = document.getElementById('text-area').value
+  const Ispalindrome = palindromeTest(pali)
+  const E = document.getElementById('result')
+  if (Ispalindrome) {
+    E.innerHTML = 'Correct, it is a Palindrome !'
+  } else {
+    E.innerHTML = 'Sorry, it is not a Palindrome !'
+  }
 }
 checkPalindrome()
