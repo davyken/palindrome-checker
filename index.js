5a6e482ce68eb580842d358199f1c7ec82a025f2
davyken <davykennang552@example.com>
2023-12-13 12:18:39 +0100

    modified content

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
-// program to check if the string is palindrome or not
-
 function palindromeTest(str) {
 
     let l = str.length - 1
 
-    
+
     for (let i = 0; i < l / 2; i++) {
 
-        
+
         if (str[i] !== str[l]) {
             return false
         }
@@ -15,13 +13,11 @@ function palindromeTest(str) {
     }
     return true
 }
-// console.log(palindrome('Johnny'))
-
-function checkPalindrome () {
+function checkPalindrome() {
     const pali = document.getElementById('text-area').value
     const Ispalindrome = palindromeTest(pali)
     const E = document.getElementById('result')
-    if (Ispalindrome){
+    if (Ispalindrome) {
         E.innerHTML = 'Correct, it is a Palindrome !'
     } else {
         E.innerHTML = 'Sorry, it is not a Palindrome !'
