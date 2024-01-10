9e6e5aec55dd21705d5d86a23d8674ffd7e79c3a
davyken <davykennang552@example.com>
2024-01-10 11:03:18 +0100

    modified content

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-function palindromeTest(str) {
+// check for input string
+function palindromeTest (str) {
   const strArr = str.split(' ').join('')
   let s = strArr.length - 1
   for (let i = 0; i < s / 2; i++) {
@@ -10,11 +11,14 @@ function palindromeTest(str) {
   return true
 }
 
-function checkPalindrome() {
-  
+function checkPalindrome () {
+  // get the values from html
   const pali = document.getElementById('text-area').value
+  // console.log(pali)
   const isPalindrome = palindromeTest(pali)
   const E = document.getElementById('result')
+  // console.log(E)
+  // the if condition to check if it's palindrome
   if (isPalindrome) {
     E.innerHTML = 'Correct, It is a  palindrome'
   } else {
